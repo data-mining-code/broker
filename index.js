5f9653df961b81e4a65b1ddc0ad7dbbd2d739f92
olivia <olivia@fastmail.com>
2018-02-23 12:31:15 +0100

    save chatlog in firebase

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const serverRouter = require('server-router')
 const r2 = require('r2')
 const exec = require('execall')
+const firebase = require('firebase')
 const http = require('http')
 const url = require('url')
 const querystring = require('querystring')
 
 const router = serverRouter()
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+}
+firebase.initializeApp(config)
+const database = firebase.database()
 
 const clients = [
   {
@@ -30,17 +40,28 @@ const clients = [
 ]
 
 router.route('GET', '/api/request', async (req, res) => {
+  const timestamp = Date.now()
   const input = querystring.parse(url.parse(req.url).query).input
   const client = clients.find(c => c.regex.test(input))
   let qs = `?client=${client.name}`
   const result = exec(client.regex, input)
+  let args = {}
   if (client.arguments) {
     client.arguments.forEach(arg => {
       if (!arg.optional && result[0].sub[arg.index] !== undefined) {
         qs = qs.concat(`&${arg.name}=${result[0].sub[arg.index]}`)
+        args[arg.name] = result[0].sub[arg.index]
       }
     })
   }
+  const obj = {
+    text: input,
+    query: {
+      client: client.name,
+      ...args
+    }
+  }
+  database.ref(`logs/${timestamp}/request`).set(obj)
 
   let response
   if (client.name === 'notfound') {
@@ -51,6 +72,9 @@ router.route('GET', '/api/request', async (req, res) => {
   res.writeHead(200, {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json'
+  })
+  database.ref(`logs/${timestamp}/response`).set({
+    text: response
   })
   res.end(JSON.stringify({text}))
 })
