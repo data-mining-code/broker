ee85dd51eef6e73f405683711d85304293aa39c0
8BitJonny <timon.christiansen@code.berlin>
2018-02-26 19:04:31 +0100

    added new clients and regex

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const database = firebase.database()
 const clients = [
   {
     "name": "stock",
-    "regex": /(?:is(?: there)?|do we have) ([\s\S]+) in stock(?: at (\w+))?/i,
+    "regex": /(?:is(?: there)?|do you have) ([\s\S]+) in stock(?: at (\w+))?/i,
     "arguments": [
       {
         "name": "product",
@@ -30,6 +30,63 @@ const clients = [
         "name": "location",
         "index": 1,
         "optional": true
+      }
+    ]
+  },
+  {
+    "name": "discount",
+    "regex": /(?:is(?: there)?|do we have) ([\s\S]+) (?:on discount|on sale)(?: at (\w+))?/i,
+    "arguments": [
+      {
+        "name": "product",
+        "index": 0
+      },
+      {
+        "name": "location",
+        "index": 1,
+        "optional": true
+      }
+    ]
+  },
+  {
+    "name": "hours",
+    "regex": /(?:(?:what|when) are you(?:r)? (?:opening hours|open))(?: at (\w+))?/i,
+    "arguments": [
+      {
+        "name": "location",
+        "index": 0,
+        "optional": true
+      }
+    ]
+  },
+  {
+    "name": "all_locations",
+    "regex": /(?:where do you have (?:shops|stores)|how many shops do you have)(?: in (\w+))?/i,
+    "arguments": [
+      {
+        "name": "location",
+        "index": 0,
+        "optional": true
+      }
+    ]
+  },
+  {
+    "name": "location",
+    "regex": /(?:(?:do you have a|is there a) (?:shop|store))(?: in (\w+))?/i,
+    "arguments": [
+      {
+        "name": "location",
+        "index": 0
+      }
+    ]
+  },
+  {
+    "name": "address",
+    "regex": /(?:where is your (?:shop|store) (?:in|at)) (\w+)?/i,
+    "arguments": [
+      {
+        "name": "location",
+        "index": 0
       }
     ]
   },
@@ -55,7 +112,6 @@ router.route('GET', '/api/request', async (req, res) => {
       }
     })
   }
-
   const obj = {
     text: input,
     query: {
@@ -63,7 +119,7 @@ router.route('GET', '/api/request', async (req, res) => {
       ...args
     }
   }
-  database.ref(`logs/${timestamp}/request`).set(obj)
+  firebase.database().ref(`logs/${timestamp}/request`).set(obj)
 
   let response
   if (client.name === 'notfound') {
@@ -78,7 +134,10 @@ router.route('GET', '/api/request', async (req, res) => {
   database.ref(`logs/${timestamp}/response`).set({
     text: response
   })
-  res.end(JSON.stringify({text}))
+  .catch(function(error) {
+    console.log(error)
+  });
+  res.end(JSON.stringify({text:response}))
 })
 
 http.createServer(router.start()).listen(3030)
